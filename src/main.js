2ef5d05902e2f392c98a9fa28389d01edafe33b0
rong-chen <rong.chen.hh@gmail.com>
2024-06-20 22:54:23 +0800

    Feat#注册添加字段

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import router from "@/router/router.js";
 import {createPinia} from "pinia";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
 let app = createApp(App);
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 app.use(createPinia())
 app.use(ElementPlus)
 app.use(router)
--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,223 +1,244 @@
 <template>
   <div class="form-container drag-mod">
-   <div class="box3d" :class="{
+    <div class="box3d" :class="{
      transformY:isRegister
    }">
-     <div class="login-box boxItem" >
-       <p class="title">胖狐用户认证中心</p>
-       <form class="form no-drag" @submit.prevent.stop="handleSubmit">
-         <input v-model="formData.username" type="text" class="input " placeholder="手机号码" />
-         <input
-             v-model="formData.password"
-             type="password"
-             class="input "
-             placeholder="密码"
-         />
-         <button class="form-btn-register form-btn loginBtn" @click.prevent="registerBtn">注册</button>
-         <button class="form-btn loginBtn" type="submit">登录</button>
-       </form>
-     </div>
-     <div class="register-box  boxItem" style="">
-       <div><img  class="backIcon" src="@/assets/back.png" @click="back" alt="返回"></div>
-       <p class="title" >胖狐用户注册中心</p>
-       <el-form :rules="rules" ref="registerFormRef" :model="registerForm"  label-width="auto">
-         <el-carousel ref="carouselRef" @change ="carouselChange" height="130px" :interval="5000" indicator-position="none" :autoplay="false"  arrow="never">
-           <el-carousel-item>
-             <div style="width: 100%">
-               <el-form-item  prop="username">
-                  <input v-model="registerForm.username" type="text" class="input " placeholder="用户名" />
-               </el-form-item>
-               <el-form-item prop="password">
-                 <input
-                     v-model="registerForm.password"
-                     type="password"
-                     class="input inputStyle"
-                     placeholder="密码"
-                 />
-               </el-form-item>
-             </div>
-           </el-carousel-item>
-           <el-carousel-item>
-             <div style="width: 100%">
-               <el-form-item prop="nickname">
-               <input  v-model="registerForm.nickname" type="text" class="input " placeholder="昵称" /></el-form-item>
-              <el-form-item  prop="phone"><input
-                   v-model="registerForm.phone"
-                   type="password"
-                   class="input inputStyle"
-                   placeholder="手机号码"
-               /></el-form-item>
-             </div>
-           </el-carousel-item>
-           <el-carousel-item>
-             <div style="width: 100%">
-               <el-form-item prop="nickname">
-                 <input  v-model="registerForm.sex" type="text" class="input " placeholder="性别" /></el-form-item>
-               <el-form-item  prop="出生日期">
-                  <input
-                   v-model="registerForm.birthday"
-                   type="password"
-                   class="input inputStyle"
-                   placeholder="手机号码"
+      <div class="login-box boxItem">
+        <p class="title">胖狐用户认证中心</p>
+        <form class="form no-drag" @submit.prevent.stop="handleSubmit">
+          <input v-model="formData.username" type="text" class="input " placeholder="手机号码"/>
+          <input
+              v-model="formData.password"
+              type="password"
+              class="input "
+              placeholder="密码"
+          />
+          <button class="form-btn-register form-btn loginBtn" @click.prevent="registerBtn">注册</button>
+          <button class="form-btn loginBtn" type="submit">登录</button>
+        </form>
+      </div>
+      <div class="register-box  boxItem" style="">
+        <div><img class="backIcon" src="@/assets/back.png" @click="back" alt="返回"></div>
+        <p class="title">胖狐用户注册中心</p>
+        <el-form :rules="rules" ref="registerFormRef" :model="registerForm" label-width="auto">
+          <el-carousel ref="carouselRef" @change="carouselChange" height="130px" :interval="5000"
+                       indicator-position="none" :autoplay="false" arrow="never">
+            <el-carousel-item>
+              <div style="width: 100%">
+                <el-form-item prop="username">
+                  <el-input v-model="registerForm.username" style="width: 300px;height:40px" placeholder="用户名"/>
+                </el-form-item>
+                <el-form-item prop="password">
+                  <el-input v-model="registerForm.password"
+                            type="password"
+                            style="width: 300px;height:40px" placeholder="密码"/>
+                </el-form-item>
+              </div>
+            </el-carousel-item>
+            <el-carousel-item>
+              <div style="width: 100%">
+                <el-form-item prop="nickname">
+                  <el-input v-model="registerForm.nickname" style="width: 300px;height:40px" placeholder="昵称"/>
+                </el-form-item>
+                <el-form-item prop="phone">
+                  <el-input v-model="registerForm.phone" style="width: 300px;height:40px" placeholder="手机号码"/>
+                </el-form-item>
+              </div>
+            </el-carousel-item>
+            <el-carousel-item>
+              <div style="
+                    width: 100%;
+                    height: 100%;
+                  "
+              >
+                <el-form-item prop="birthday">
+                  <el-date-picker
+                      v-model="registerForm.birthday"
+                      type="date"
+                      style="    width: 300px;height: 40px"
+                      placeholder="出生日期"
+                      format="YYYY/MM/DD"
+                      value-format="x"
                   />
                 </el-form-item>
-             </div>
-           </el-carousel-item>
-           <el-carousel-item>
-             <div style="width: 100%">
-               <el-form-item prop="email">
-                <input  v-model="registerForm.email" type="text" class="input " placeholder="邮箱" />
-               </el-form-item>
-               <div style="display: flex">
-                 <el-form-item prop="code">
-                   <input
-                     v-model="registerForm.code"
-                     type="password"
-                     class="input inputStyle"
-                     style="width: 200px"
-                     placeholder="验证码"
-                    /></el-form-item>
-                   <button @click.prevent="receiveCode" style="margin-left: 10px;height: 40px" class="form-btn loginBtn inputStyle">
-                       验证码
-                   </button>
-               </div>
-             </div>
-           </el-carousel-item>
-         </el-carousel>
-         <button class="form-btn loginBtn inputStyle"  v-if="carouselIndex !== 3"  @click.prevent="next()">下一步</button>
-         <button class="form-btn loginBtn inputStyle" v-if="carouselIndex === 3" @click.prevent.stop="registerHandleSubmit">完成</button>
-         <button class="form-btn-register form-btn loginBtn inputStyle" v-if="carouselIndex !== 0"   @click.prevent="pre()">上一步</button>
-       </el-form>
-     </div>
-   </div>
+                <el-form-item prop="sex">
+                  <el-select v-model="registerForm.sex" placeholder="Select" style="width: 300px;height: 40px">
+                    <el-option
+                        v-for="item in [{label:'男',value:'男'},{label:'女',value:'女'}]"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value"
+                    />
+                  </el-select>
+
+                </el-form-item>
+
+              </div>
+            </el-carousel-item>
+            <el-carousel-item>
+              <div style="width: 100%">
+                <el-form-item prop="email">
+                  <el-input v-model="registerForm.email" style="width: 300px;height:40px" placeholder="邮箱"/>
+                </el-form-item>
+                <div style="display: flex;justify-content: space-between">
+                  <el-form-item prop="code">
+                    <el-input
+                        v-model="registerForm.code"
+                        style="height:40px"
+                        placeholder="验证码"
+                    />
+                  </el-form-item>
+                  <el-button type="primary" icon="Iphone" @click.prevent="receiveCode" style="margin-left: 10px;width: 100px;height: 40px"
+                          class=" loginBtn ">
+                    验证码
+                  </el-button>
+                </div>
+              </div>
+            </el-carousel-item>
+          </el-carousel>
+          <button class="form-btn loginBtn inputStyle" v-if="carouselIndex !== 3" @click.prevent="next()">下一步
+          </button>
+          <button class="form-btn loginBtn inputStyle" v-if="carouselIndex === 3"
+                  @click.prevent.stop="registerHandleSubmit">完成
+          </button>
+          <button class="form-btn-register form-btn  inputStyle" style="margin-top: 10px" v-if="carouselIndex !== 0"
+                  @click.prevent="pre()">上一步
+          </button>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
 import {onMounted, ref} from 'vue'
-import {Login , Register} from "@/api/login.js";
-import {useRoute,useRouter} from "vue-router";
+import {Login, Register} from "@/api/login.js";
+import {useRoute, useRouter} from "vue-router";
 import {SendEmailCode} from "@/api/register.js";
 import {ElMessage} from "element-plus";
-const registerFormRef =ref(null)
+
+const registerFormRef = ref(null)
 let registerForm = ref({
-  "username":"",
-  "password":"",
-  "phone":"",
-  "nickname":"",
-  "email":"",
-  "code":"",
-  "sex":"",
-  "birthday":""
+  "username": "",
+  "password": "",
+  "phone": "",
+  "nickname": "",
+  "email": "",
+  "code": "",
+  "sex": "",
+  "birthday": ""
 })
-let formData=ref({
-  username:"",
-  password:"",
-  redirect_url:"",
-  state:"",
-  client_id:""
+let formData = ref({
+  username: "",
+  password: "",
+  redirect_url: "",
+  state: "",
+  client_id: ""
 })
 
-function validateUsername(rule, value, callback){
+function validateUsername(rule, value, callback) {
   const regex = /^\d{6,}$/;
-  regex.test(value) ? callback() :callback(new Error("请输入六位或者六位以上的数字"))
-  return
+  regex.test(value) ? callback() : callback(new Error("请输入六位或者六位以上的数字"))
+
 }
+
 function validateChinesePhoneNumber(rule, value, callback) {
   // 正则表达式，用于匹配中国手机号码
   const regex = /^1[3-9]\d{9}$/;
-  regex.test(value) ? callback() :callback(new Error("请输入正确的手机号码(中国)"))
+  regex.test(value) ? callback() : callback(new Error("请输入正确的手机号码(中国)"))
 }
+
 function validateEmail(rule, value, callback) {
   // 正则表达式，用于匹配标准的电子邮件地址格式
   const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  regex.test(value) ? callback() :callback(new Error("请输入正确的邮箱地址"))
+  regex.test(value) ? callback() : callback(new Error("请输入正确的邮箱地址"))
 }
 
 const rules = ref({
   username: [
-    { required: true, message: '请输入用户名', trigger: 'blur' },
-    { validator: validateUsername, trigger: 'blur'}
+    {required: true, message: '请输入用户名', trigger: 'blur'},
+    {validator: validateUsername, trigger: 'blur'}
   ],
-  password:[
-    { required: true, message: '请输入密码', trigger: 'blur' }
+  password: [
+    {required: true, message: '请输入密码', trigger: 'blur'}
   ],
-  phone:[
-    { required: true, message: '请输入手机号码', trigger: 'blur' },
-    { validator: validateChinesePhoneNumber, trigger: 'blur'}
+  phone: [
+    {required: true, message: '请输入手机号码', trigger: 'blur'},
+    {validator: validateChinesePhoneNumber, trigger: 'blur'}
   ],
-  nickname:[
-    { required: true, message: '请输入用户昵称', trigger: 'blur' },
+  nickname: [
+    {required: true, message: '请输入用户昵称', trigger: 'blur'},
   ],
-  email:[
-    { required: true, message: '请输入邮箱', trigger: 'blur' },
+  email: [
+    {required: true, message: '请输入邮箱', trigger: 'blur'},
     {validator: validateEmail, trigger: 'blur'}
   ],
-  code:[
-    { required: true, message: '请输入邮箱验证码', trigger: 'blur' },
+  code: [
+    {required: true, message: '请输入邮箱验证码', trigger: 'blur'},
   ],
-  sex:[
-    { required: true, message: '请输入邮箱验证码', trigger: 'blur' },
+  sex: [
+    {required: true, message: '请输入性别', trigger: 'blur'},
   ],
-  birthday:[
-    { required: true, message: '请输入邮箱验证码', trigger: 'blur' },
+  birthday: [
+    {required: true, message: '请输入选择出生日期', trigger: 'blur'},
   ],
 })
 
-const carouselRef =ref(null)
-const back=()=>{
-  isRegister.value=false
+const carouselRef = ref(null)
+const back = () => {
+  isRegister.value = false
   carouselRef.value.setActiveItem(0)
   registerForm.value = {
-    "username":"",
-    "password":"",
-    "phone":"",
-    "nickname":"",
-    "email":"",
-    "code":""
+    "username": "",
+    "password": "",
+    "phone": "",
+    "nickname": "",
+    "email": "",
+    "code": ""
   }
-  email.value=""
+  email.value = ""
 }
 
-onMounted(()=>{
+onMounted(() => {
   const hash = window.location.hash.substring(1);
   const hashParams = new URLSearchParams(hash);
-  formData.value.client_id= hashParams.get('client_id') || route.query.client_id;
-  formData.value.state= hashParams.get('state') || route.query.state;
+  formData.value.client_id = hashParams.get('client_id') || route.query.client_id;
+  formData.value.state = hashParams.get('state') || route.query.state;
   formData.value.redirect_url = hashParams.get('redirect_url') || route.query.redirect_url;
 })
 let email = ref("")
-const receiveCode= async ()=>{
-    if (!registerFormRef.value) return
-    await registerFormRef.value.validateField(['email'], async (valid) => {
-      if (valid) {
-        let {code} = await SendEmailCode({
-          email:registerForm.value.email
-        })
-        if(code === 200){
-          email.value =registerForm.value.email
-          ElMessage.success("发送成功")
-        }
+const receiveCode = async () => {
+  if (!registerFormRef.value) return
+  await registerFormRef.value.validateField(['email'], async (valid) => {
+    if (valid) {
+      let {code} = await SendEmailCode({
+        email: registerForm.value.email
+      })
+      if (code === 200) {
+        email.value = registerForm.value.email
+        ElMessage.success("发送成功")
       }
-    })
+    }
+  })
 
 }
 
-const ruleList =ref({
-  "0":['username','password'],
-  "1":['nickname','phone'],
-  "2":['email','code'],
+const ruleList = ref({
+  "0": ['username', 'password'],
+  "1": ['nickname', 'phone'],
+  "2": ["sex", "birthday"],
+  "3": ['email', 'code'],
 })
 
 
-const registerHandleSubmit= async ()=>{
+const registerHandleSubmit = async () => {
   if (!registerFormRef.value) return
-  await registerFormRef.value.validate( async (valid, fields) => {
+  await registerFormRef.value.validate(async (valid, fields) => {
     if (valid) {
-      if(email.value !== registerForm.value.email){
+      if (email.value !== registerForm.value.email) {
         alert("修改邮箱请重新获取验证码")
-      }else{
+      } else {
         let resp = await Register(registerForm.value)
-        if(resp.code ===200){
+        if (resp.code === 200) {
           back()
           ElMessage.success("注册成功")
         }
@@ -226,14 +247,14 @@ const registerHandleSubmit= async ()=>{
   })
 }
 
-let carouselIndex =ref(0)
-const carouselChange = (index)=>{
-  carouselIndex.value =index
+let carouselIndex = ref(0)
+const carouselChange = (index) => {
+  carouselIndex.value = index
 }
 
-const next = async ()=>{
+const next = async () => {
   if (!registerFormRef.value) return
-  await registerFormRef.value.validateField(ruleList.value[carouselIndex.value],(valid) => {
+  await registerFormRef.value.validateField(ruleList.value[carouselIndex.value], (valid) => {
     if (valid) {
       carouselRef.value.next()
     }
@@ -241,37 +262,43 @@ const next = async ()=>{
 
 
 }
-const pre =()=>{
-  carouselRef.value.setActiveItem(carouselIndex.value -1)
+const pre = () => {
+  carouselRef.value.setActiveItem(carouselIndex.value - 1)
 }
 
-const registerBtn =()=>{
-  isRegister.value =true
+const registerBtn = () => {
+  isRegister.value = true
 }
-let isRegister=ref(false)
+let isRegister = ref(false)
 const route = useRoute()
 const router = useRouter()
 const handleSubmit = async (event) => {
-  if(formData.value.username && formData.value.password){
-    if(!formData.value.redirect_url){
+  if (formData.value.username && formData.value.password) {
+    if (!formData.value.redirect_url) {
       await router.push({
         path: "/NotFound",
       })
-    }else{
+    } else {
       await Login(formData.value)
     }
-  }else{
+  } else {
     alert("请输入账号和密码")
   }
 }
 
 </script>
+<style>
+.form-container .el-select__wrapper {
+  height: 40px;
+}
+
+</style>
 <style scoped>
-.login-box{
+.login-box {
   z-index: 2;
 }
 
-.boxItem{
+.boxItem {
   position: absolute;
   width: 100%;
   height: 100%;
@@ -279,26 +306,29 @@ const handleSubmit = async (event) => {
   padding: 10px 30px 40px;
   box-sizing: border-box;
 }
+
 .transformY {
   transform: rotateY(180deg) !important;
   z-index: 4;
 }
-.box3d{
+
+.box3d {
   position: absolute;
   background-color: rgba(255, 255, 255, 0.7);
   transform-style: preserve-3d;
   transform: rotateY(0deg);
   transition: transform 0.6s;
   box-shadow: 1px 1px 10px #ececec;
-  border:1px solid #ececec;
+  border: 1px solid #ececec;
   width: 360px;
   height: 400px;
 }
 
-.register-box{
+.register-box {
   transform: rotateY(180deg);
   z-index: 1;
 }
+
 .form-container {
   width: 100%;
   height: 100vh;
@@ -307,7 +337,7 @@ const handleSubmit = async (event) => {
   justify-content: center;
   position: relative;
   background-image: url(@/assets/loginBackground.jpg);
-  background-size: 100% ,100%;
+  background-size: 100%, 100%;
   border-radius: 10px;
   padding: 20px 30px;
   background-position: center;
@@ -315,10 +345,11 @@ const handleSubmit = async (event) => {
   box-shadow: rgba(0, 0, 0, 0.35) 0 5px 15px;
   box-sizing: border-box;
 }
-.inputStyle{
-  margin-top: 18px;
+
+.inputStyle {
   width: 100%;
 }
+
 .title {
   text-align: center;
   font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva,
@@ -331,12 +362,14 @@ const handleSubmit = async (event) => {
   align-items: center;
   justify-content: center;
 }
-.backIcon{
+
+.backIcon {
   position: absolute;
   top: 20px;
   left: 20px;
-  width:30px
+  width: 30px
 }
+
 .form {
   width: 100%;
   display: flex;
@@ -358,7 +391,6 @@ const handleSubmit = async (event) => {
 }
 
 
-
 .form-btn {
   padding: 10px 15px;
   font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva,
@@ -373,11 +405,12 @@ const handleSubmit = async (event) => {
   font-weight: bold;
 }
 
-.form-btn-register{
+.form-btn-register {
   border: 1px solid teal;
   background-color: white;
   color: black;
 }
+
 .form-btn:active {
   box-shadow: none;
 }
